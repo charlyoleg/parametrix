48f86455d2966f1fe2e2667d91f8c044f2af7b3e
charles <charles.braquet@e.email>
2023-02-27 23:14:20 +0100

    refactor cad list

--- src/lib/CadEcosystem.svelte
+++ src/lib/CadEcosystem.svelte
@@ -1,12 +1,11 @@
+<script lang="ts">
+	import MyUl from '$lib/MyUl.svelte';
+</script>
+
 <ul>
 	<li class="list-header">Open CAD Ecosystem</li>
 	<li>
-		<input type="checkbox" id="toggle_2d" class="toggle" checked />
-		<label for="toggle_2d" class="label"
-			><div class="arrow" />
-			2D drawing</label
-		>
-		<ul class="nested">
+		<MyUl checkboxId="toggle_2d" labelText="2D drawing">
 			<li>
 				<a href="https://librecad.org/">LibreCAD</a>
 				<small>A community fork of QCAD 2.0</small>
@@ -23,69 +22,54 @@
 				<a href="https://github.com/VVS1864/SAMoCAD">SAMoCAD</a>
 				<small>An unmaintained python2/OpenGL tool</small>
 			</li>
-		</ul>
+		</MyUl>
 	</li>
 	<li>
-		<input type="checkbox" id="toggle_3d" class="toggle" checked />
-		<label for="toggle_3d" class="label"
-			><div class="arrow" />
-			3D drawing</label
-		>
-		<ul class="nested">
-			<li>Libraries for 3D engine</li>
+		<MyUl checkboxId="toggle_3d" labelText="3D drawing">
+			<li>Classified by 3D geometry libraries</li>
 			<li>
-				<input type="checkbox" id="toggle_opengl" class="toggle" checked />
-				<label for="toggle_opengl" class="label"
-					><div class="arrow" />
-					OpenGL</label
-				>
-				<a href="https://www.opengl.org/">OpenGL.org</a>
-				<small>The rendering library of Khronos</small>
-				<ul class="nested">
+				<MyUl checkboxId="toggle_inhouse" labelText="In-house solutions">
 					<li>
 						<a href="https://www.gcad3d.org/">gCAD3D</a>
 						<small>The lightweight 3D modeling based on GTK and OpenGL</small>
 					</li>
-				</ul>
+					<li>
+						<a href="https://github.com/BRL-CAD/brlcad">BRL-CAD</a>
+						<small
+							>An open source cross-platform solid modeling system. Originated from
+							American defense industry</small
+						>
+					</li>
+				</MyUl>
 			</li>
 			<li>
-				<input type="checkbox" id="toggle_cgal" class="toggle" checked />
-				<label for="toggle_cgal" class="label"
-					><div class="arrow" />
-					CGAL</label
-				>
-				<a href="https://www.cgal.org/">CGAL.org</a>
-				<small
-					>The C++ Computational Geometry Algorithms Library founded by European research
-					instituts</small
-				>
-				<ul class="nested">
+				<MyUl checkboxId="toggle_cgal" labelText="CGAL">
+					<a slot="top1" href="https://www.cgal.org/">CGAL.org</a>
+					<small slot="top2"
+						>The C++ Computational Geometry Algorithms Library founded by European
+						research instituts</small
+					>
 					<li>
-						<a href="https://openscad.org/">OpenSCAD</a>
-						<small>A pioneer scripting tool for 3D modeling</small>
-						<ul class="nested sub">
+						<MyUl checkboxId="toggle_openscad" labelText="OpenSCAD">
+							<a slot="top1" href="https://openscad.org/">OpenSCAD.org</a>
+							<small slot="top2">A pioneer scripting tool for 3D modeling</small>
 							<li>
 								<a href="https://github.com/janssen86/OpenSCAD-Getriebebibliothek"
 									>OpenSCAD Gear</a
 								>
 								<small>Gear generator based on OpenSCAD</small>
 							</li>
-						</ul>
+						</MyUl>
 					</li>
-				</ul>
+				</MyUl>
 			</li>
 			<li>
-				<input type="checkbox" id="toggle_occ" class="toggle" checked />
-				<label for="toggle_occ" class="label"
-					><div class="arrow" />
-					OpenCascade</label
-				>
-				<a href="https://dev.opencascade.org/">OpenCascade.org</a>
-				<small
-					>The world famous open source 3D modeling library. Originated from France
-					defense industry</small
-				>
-				<ul class="nested">
+				<MyUl checkboxId="toggle_occ" labelText="OpenCascade">
+					<a slot="top1" href="https://dev.opencascade.org/">OpenCascade.org</a>
+					<small slot="top2"
+						>The world famous open source 3D modeling library. Originated from France
+						defense industry</small
+					>
 					<li>
 						<a href="https://www.freecad.org/">FreeCAD</a>
 						<small>Powerful GUI based on Qt</small>
@@ -95,9 +79,11 @@
 						<small>Light weight GUI based on GTK</small>
 					</li>
 					<li>
-						<a href="https://github.com/tpaviot/pythonocc-core">pythonOCC</a>
-						<small>Python binding of OpenCascade based on SWIG</small>
-						<ul class="nested sub">
+						<MyUl checkboxId="toggle_pythonocc" labelText="pythonOCC">
+							<a slot="top1" href="https://github.com/tpaviot/pythonocc-core"
+								>pythonOCC</a
+							>
+							<small slot="top2">Python binding of OpenCascade based on SWIG</small>
 							<li>
 								<a href="https://github.com/e-dub/pygear">pythonOCC Gear</a>
 								<small>Gear generator based on pythonOCC</small>
@@ -108,19 +94,19 @@
 									>A python library for parametric 3D modeling based on pythonOCC</small
 								>
 							</li>
-						</ul>
+						</MyUl>
 					</li>
 					<li>
 						<a href="https://github.com/trelau/pyOCCT">pyOCCT</a>
 						<small>Python binding of OpenCascade based on pybind11</small>
 					</li>
 					<li>
-						<a href="https://cadquery.readthedocs.io">CadQuery</a>
-						<small
-							>Python module based on OpenCascade for building parametric 3D CAD
-							models</small
-						>
-						<ul class="nested sub">
+						<MyUl checkboxId="toggle_cq" labelText="CadQuery">
+							<a slot="top1" href="https://cadquery.readthedocs.io">CQ</a>
+							<small slot="top2"
+								>Python module based on OpenCascade for building parametric 3D CAD
+								models</small
+							>
 							<li>
 								<a href="https://github.com/meadiode/cq_gears">CQ Gear</a>
 								<small>Gear generator based on CadQuery</small>
@@ -129,15 +115,15 @@
 								<a href="https://github.com/michaelgale/cq-kit">CQ Kit</a>
 								<small>Helper python library for CadQuery</small>
 							</li>
-						</ul>
+						</MyUl>
 					</li>
 					<li>
-						<a href="https://ocjs.org/">OpenCascade.js</a>
-						<small
-							>Port of the OpenCascade CAD library to JavaScript and WebAssembly via
-							Emscripten</small
-						>
-						<ul class="nested sub">
+						<MyUl checkboxId="toggle_ocjs" labelText="OpenCascade.js">
+							<a slot="top1" href="https://ocjs.org/">OCJS</a>
+							<small slot="top2"
+								>Port of the OpenCascade CAD library to JavaScript and WebAssembly
+								via Emscripten</small
+							>
 							<li>
 								<a href="https://github.com/sgenoud/replicad">Replicad</a>
 								<small>A js-scripting library for 3D modeling</small>
@@ -152,38 +138,37 @@
 								>
 								<small>An other js-scripting library for 3D modeling</small>
 							</li>
-						</ul>
+						</MyUl>
 					</li>
-				</ul>
+				</MyUl>
 			</li>
 			<li>
-				<a href="https://github.com/jscad/OpenJSCAD.org">OpenJsCAD</a>
-				<small>Web based solid modeling in Javascript</small>
+				<MyUl checkboxId="toggle_jscad" labelText="OpenJsCAD">
+					<a slot="top1" href="https://github.com/jscad/OpenJSCAD.org">OpenJSCAD.org</a>
+					<small slot="top2">Web based solid modeling in Javascript</small>
+					<li>
+						<a href="https://joostn.github.io/OpenJsCad/">OpenJsCAD predecessor</a>
+						<small>The unmaintained original version of OpenJsCAD</small>
+					</li>
+					<li>
+						<a href="https://garyhodgson.github.io/openscad.net/">OpenSCAD.net</a>
+						<small>Partial port of OpenSCAD for the web. Powered by OpenJsCad</small>
+					</li>
+				</MyUl>
 			</li>
 			<li>
-				<a href="https://joostn.github.io/OpenJsCad/">OpenJsCAD predecessor</a>
-				<small>The unmaintained original version of OpenJsCAD</small>
+				<MyUl checkboxId="toggle_libfive" labelText="libfive">
+					<a slot="top1" href="https://libfive.com/">libfive</a>
+					<small slot="top2"
+						>A library for parametric solid modeling and generative design</small
+					>
+					<li>
+						<a href="https://github.com/mkeeter/antimony">Antimony</a>
+						<small>CAD from a parallel universe</small>
+					</li>
+				</MyUl>
 			</li>
-			<li>
-				<a href="https://garyhodgson.github.io/openscad.net/">OpenSCAD.net</a>
-				<small>Partial port of OpenSCAD for the web. Powered by OpenJsCad</small>
-			</li>
-			<li>
-				<a href="https://github.com/BRL-CAD/brlcad">BRL-CAD</a>
-				<small
-					>A powerful open source cross-platform solid modeling system. Originated from
-					American defense industry</small
-				>
-			</li>
-			<li>
-				<a href="https://libfive.com/">libfive</a>
-				<small>A library for parametric solid modeling and generative design</small>
-			</li>
-			<li>
-				<a href="https://github.com/mkeeter/antimony">Antimony</a>
-				<small>CAD from a parallel universe</small>
-			</li>
-		</ul>
+		</MyUl>
 	</li>
 </ul>
 
@@ -194,35 +179,6 @@
 		list-style-type: none;
 		margin: 0;
 		padding: 0;
-	}
-	ul.nested {
-		padding-left: 2rem;
-		display: none;
-	}
-	ul.nested.sub {
-		display: block;
-	}
-	input.toggle:checked ~ ul.nested {
-		display: block;
-	}
-	input.toggle {
-		display: none;
-	}
-	$arrow-size: 0.4rem;
-	div.arrow {
-		display: inline-block;
-		width: 0;
-		height: 0;
-		border-top: $arrow-size solid transparent;
-		border-bottom: $arrow-size solid transparent;
-		border-left: (1.8 * $arrow-size) solid DarkOrange;
-		margin-left: 0.5rem;
-	}
-	input.toggle:checked ~ label > div.arrow {
-		border-left: $arrow-size solid transparent;
-		border-right: $arrow-size solid transparent;
-		border-top: (1.8 * $arrow-size) solid colors.$arrow;
-		border-bottom: 0;
 	}
 	li.list-header {
 		font-size: 1rem;
---
+++ src/lib/MyUl.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	export let checkboxId;
+	export let labelText;
+</script>
+
+<input type="checkbox" id={checkboxId} class="toggle" checked />
+<label for={checkboxId} class="label"
+	><div class="arrow" />
+	{labelText}</label
+>
+<slot name="top1" />
+<slot name="top2" />
+<ul class="nested">
+	<slot>dbg492</slot>
+</ul>
+
+<style lang="scss">
+	@use '$lib/style/colors.scss';
+
+	input.toggle {
+		display: none;
+	}
+	ul.nested {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		padding-left: 2rem;
+		display: none;
+	}
+	input.toggle:checked ~ ul.nested {
+		display: block;
+	}
+	$arrow-size: 0.4rem;
+	div.arrow {
+		display: inline-block;
+		width: 0;
+		height: 0;
+		border-top: $arrow-size solid transparent;
+		border-bottom: $arrow-size solid transparent;
+		border-left: (1.8 * $arrow-size) solid DarkOrange;
+		margin-left: 0.5rem;
+	}
+	input.toggle:checked ~ label > div.arrow {
+		border-left: $arrow-size solid transparent;
+		border-right: $arrow-size solid transparent;
+		border-top: (1.8 * $arrow-size) solid colors.$arrow;
+		border-bottom: 0;
+	}
+</style>
