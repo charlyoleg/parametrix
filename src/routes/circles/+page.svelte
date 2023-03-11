4fffcd3d2fa2ce62941de1d484e3653166837660
charles <charles.braquet@e.email>
2023-03-11 13:03:16 +0100

    starting with canvas

--- src/routes/circles/+page.svelte
+++ src/routes/circles/+page.svelte
@@ -1,7 +1,20 @@
+<script lang="ts">
+	//import { base } from '$lib/geom/culcal.ts';
+	//const p1 = Point(0, 0);
+	//const p2 = Point(5, 5);
+	//const l1 = Line(p1, p2);
+	//const a1 = Arc(p1, p2, 1);
+	//const elemCanvas = document.getElementById('abc');
+	//drawCanvas(elemCanvas, l1);
+</script>
+
 <h2>Circles</h2>
 <article>A circle with circle holes.</article>
+<canvas id="abc" width="400" height="400" />
 
-<style>
+<style lang="scss">
+	@use '$lib/style/colors.scss';
+
 	h2 {
 		margin: 0;
 	}
