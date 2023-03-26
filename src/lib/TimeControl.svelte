21521b8585b3259c0857a074c6d5dc68ce4afef0
charles <charles.braquet@e.email>
2023-03-26 01:37:35 +0100

    geometry time control oops

---
+++ src/lib/TimeControl.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+
+	export const tMax = 10;
+	export const tStep = 0.1;
+	export const tUpdate = 500; // ms
+	export let simTime = 0;
+
+	let intervalID: ReturnType<typeof setTimeout> | null = null;
+	let speed = 0;
+	let inc: number;
+	const speedMax = 4;
+	function clearInterval2() {
+		if (intervalID !== null) {
+			clearInterval(intervalID);
+			intervalID = null;
+		}
+	}
+	function simRoutine() {
+		if (inc < 0 && simTime <= 0) {
+			simTime = tMax;
+		} else if (inc > 0 && simTime >= tMax) {
+			simTime = 0;
+		} else {
+			simTime += inc;
+		}
+	}
+	function setInterval2() {
+		const delay = tUpdate / 2 ** (Math.abs(speed) - 1);
+		inc = speed < 0 ? -tStep : tStep;
+		intervalID = setInterval(simRoutine, delay);
+		//console.log(`dbg909: ${delay} ======`);
+	}
+	function simZero() {
+		clearInterval2();
+		simTime = 0;
+		speed = 0;
+	}
+	function simDecrem() {
+		clearInterval2();
+		speed = 0;
+		inc = -tStep;
+		simRoutine();
+		//console.log(`dbg059: ${simTime}`);
+	}
+	function simIncrem() {
+		clearInterval2();
+		speed = 0;
+		inc = tStep;
+		simRoutine();
+		//console.log(`dbg069: ${simTime}`);
+	}
+	function simPlayBackward() {
+		clearInterval2();
+		speed = Math.max(speed - 1, -speedMax);
+		setInterval2();
+		//console.log(`dbg954: ${speed} ======`);
+	}
+	function simPlayForward() {
+		clearInterval2();
+		speed = Math.min(speed + 1, speedMax);
+		setInterval2();
+		//console.log(`dbg961: ${speed} ======`);
+	}
+	function simPause() {
+		clearInterval2();
+		speed = 0;
+	}
+	onDestroy(() => {
+		clearInterval2;
+	});
+</script>
+
+<nav>
+	<button on:click={simZero}>0</button>
+	<button on:click={simPlayBackward}>&lt-</button>
+	<button on:click={simDecrem}>|&lt</button>
+	<button on:click={simPause}>||</button>
+	<button on:click={simIncrem}>&gt|</button>
+	<button on:click={simPlayForward}>-&gt</button>
+</nav>
+
+<style lang="scss">
+	@use '$lib/style/colors.scss';
+
+	nav > button {
+		background-color: lightBlue;
+	}
+</style>
