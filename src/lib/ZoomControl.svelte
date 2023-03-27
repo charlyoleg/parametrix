1cf3d436c6d98d81a0a81d495fa2cb8e983304c8
charles <charles.braquet@e.email>
2023-03-27 21:35:15 +0200

    zoom style

--- src/lib/ZoomControl.svelte
+++ src/lib/ZoomControl.svelte
@@ -11,19 +11,90 @@
 </script>
 
 <nav>
-	<button on:click={() => theClick('zoomInit')}>Init</button>
-	<button on:click={() => theClick('zoomIn')}>In</button>
-	<button on:click={() => theClick('zoomOut')}>Out</button>
-	<button on:click={() => theClick('moveLeft')}>Left</button>
-	<button on:click={() => theClick('moveRight')}>Right</button>
-	<button on:click={() => theClick('moveUp')}>Up</button>
-	<button on:click={() => theClick('moveDown')}>Down</button>
+	<button on:click={() => theClick('zoomInit')}><div class="zero" /></button>
+	<button on:click={() => theClick('zoomIn')}><div class="zoomin" /></button>
+	<button on:click={() => theClick('zoomOut')}><div class="zoomout" /></button>
+	<button on:click={() => theClick('moveLeft')}><div class="arrowleft" /></button>
+	<button on:click={() => theClick('moveRight')}><div class="arrowright" /></button>
+	<button on:click={() => theClick('moveUp')}><div class="arrowup" /></button>
+	<button on:click={() => theClick('moveDown')}><div class="arrowdown" /></button>
 </nav>
 
 <style lang="scss">
 	@use '$lib/style/colors.scss';
 
 	nav > button {
-		background-color: lightGreen;
+		height: 2rem;
+		width: 2rem;
+		border-style: solid;
+		border-width: 0.1rem;
+		border-radius: 0.2rem;
+		border-color: colors.$zoom-sign;
+		margin: 0.1rem;
+		background-color: colors.$zoom-bg;
+	}
+	$zero-size: 0.8rem;
+	nav > button > div.zero {
+		height: $zero-size;
+		width: $zero-size;
+		border-style: solid;
+		border-width: 0.3rem;
+		border-radius: 1rem;
+		border-color: colors.$zoom-sign;
+		margin: auto;
+		background-color: transparent;
+	}
+	nav > button > div.zoomin {
+		height: $zero-size;
+		width: $zero-size;
+		border-style: solid;
+		border-width: 0.3rem;
+		border-radius: 0;
+		border-color: colors.$zoom-other;
+		margin: auto;
+		background-color: colors.$zoom-sign;
+	}
+	nav > button > div.zoomout {
+		height: $zero-size;
+		width: $zero-size;
+		border-style: solid;
+		border-width: 0.3rem;
+		border-radius: 0;
+		border-color: colors.$zoom-sign;
+		margin: auto;
+		background-color: colors.$zoom-other;
+	}
+	$arrow-size: 0.7rem;
+	nav > button > div.arrowleft {
+		width: 0;
+		height: 0;
+		border-top: $arrow-size solid transparent;
+		border-bottom: $arrow-size solid transparent;
+		border-right: (1.8 * $arrow-size) solid colors.$zoom-sign;
+		margin: auto;
+	}
+	nav > button > div.arrowright {
+		width: 0;
+		height: 0;
+		border-top: $arrow-size solid transparent;
+		border-bottom: $arrow-size solid transparent;
+		border-left: (1.8 * $arrow-size) solid colors.$zoom-sign;
+		margin: auto;
+	}
+	nav > button > div.arrowup {
+		width: 0;
+		height: 0;
+		border-left: $arrow-size solid transparent;
+		border-right: $arrow-size solid transparent;
+		border-bottom: (1.8 * $arrow-size) solid colors.$zoom-sign;
+		margin: auto;
+	}
+	nav > button > div.arrowdown {
+		width: 0;
+		height: 0;
+		border-left: $arrow-size solid transparent;
+		border-right: $arrow-size solid transparent;
+		border-top: (1.8 * $arrow-size) solid colors.$zoom-sign;
+		margin: auto;
 	}
 </style>
