787a2d8bb598b01c01fd90b9a9f65f7db7ed43c4
charles <charles.braquet@e.email>
2023-04-04 22:24:10 +0200

    styling page top

--- src/routes/circles/+page.svelte
+++ src/routes/circles/+page.svelte
@@ -49,6 +49,12 @@
 	@use '$lib/style/colors.scss';
 
 	h1 {
-		margin: 1rem;
+		margin: 1rem 1rem 0;
+	}
+	article {
+		font-size: 1rem;
+		font-weight: 400;
+		font-style: italic;
+		margin: 0 1rem 2rem;
 	}
 </style>
