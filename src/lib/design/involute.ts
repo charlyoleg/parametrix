64228c95ea10a683f05812f0fac78df618752449
charles <charles.braquet@e.email>
2023-06-09 20:54:13 +0200

    docs on point of contact

--- src/lib/design/involute.ts
+++ src/lib/design/involute.ts
@@ -31,13 +31,17 @@ class Involute {
 			throw `err730: involute parameter au ${au} is larger than PI/2`;
 		}
 		const sign = this.angleSign();
-		const rw = this.initial_angle + sign * (au - Math.atan(au));
+		const rw = sign * (au - Math.atan(au));
 		return rw;
+	}
+	w2FromU(au: number): number {
+		const rw2 = this.initial_angle + this.wFromU(au);
+		return rw2;
 	}
 	// Point C of the involute in cartesian coordinates
 	ptc(au: number): Array<number> {
 		const lcl = this.lFromU(au);
-		const lcw = this.wFromU(au);
+		const lcw = this.w2FromU(au);
 		const ptcx = this.base_center_x + Math.cos(lcw) * lcl;
 		const ptcy = this.base_center_y + Math.sin(lcw) * lcl;
 		return [ptcx, ptcy];
@@ -73,15 +77,27 @@ class Involute {
 		return itu;
 	}
 	// line of action
-	// acc: angle of center-center line
-	// ap: angle of pressue relative to the center-center line
-	// ar: angle of rotation from the start of the line of action
+	// acc: angle of center-center line (rad)
+	// ap: angle of pressue relative to the center-center line (rad)
+	// aj: angle of rotation from the start of the line of action (rad)
+	// sJ: angular speed of rotations (rad/s)
 	// return
 	// x,y of point C
 	// vax, vay of speed vector of point C in line of action reference
-	//laptc(acc: number, ap: number, ar: number): Array<number> {
-	//
-	//}
+	laptc(acc: number, ap: number, aj: number, sJ: number): Array<number> {
+		if (Math.sign(ap) * this.angleSign() < 0) {
+			throw `err904: Invole.laptc angle-of-pressure ${ap} not compatible with right_nleft ${this.right_nleft}`;
+		}
+		const C0a = acc + ap;
+		const C0x = this.base_center_x + Math.cos(C0a) * this.base_radius;
+		const C0y = this.base_center_y + Math.sin(C0a) * this.base_radius;
+		const C2a = C0a - (this.angleSign() * Math.PI) / 2;
+		const lBC = aj * this.base_radius;
+		const C2x = C0x + Math.cos(C2a) * lBC;
+		const C2y = C0y + Math.sin(C2a) * lBC;
+		const lL = this.lFromU(aj);
+		return [C0x, C0y];
+	}
 }
 
 function involute(cx: number, cy: number, br: number, ia: number, rnl: boolean): Involute {
