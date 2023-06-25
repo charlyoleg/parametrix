52f677af6e1b8287a72498011e486c6d8b43ae15
charles <charles.braquet@e.email>
2023-06-25 09:16:47 +0200

    re-export type seems to be implicitly done by the js re-export

--- src/lib/geom/geom.ts
+++ src/lib/geom/geom.ts
@@ -1,13 +1,13 @@
 // geom.ts
 
-import type { Point, tContour, tLayers, Figure } from './figure';
-export type { Point, tContour, tLayers, Figure };
-//export type * from "./figure";
+//import type { Point, tContour, tLayers, Figure } from './figure';
+//export type { Point, tContour, tLayers, Figure };
+//export type * from './figure';
 export * from './figure';
 
-import type { tParamDef, tParamVal, tAllVal, tGeom, tGeomFunc, tPageDef } from './aaParamGeom';
-export type { tParamDef, tParamVal, tAllVal, tGeom, tGeomFunc, tPageDef };
-//export type * from "./aaParamGeom";
+//import type { tParamDef, tParamVal, tAllVal, tGeom, tGeomFunc, tPageDef } from './aaParamGeom';
+//export type { tParamDef, tParamVal, tAllVal, tGeom, tGeomFunc, tPageDef };
+//export type * from './aaParamGeom';
 export * from './aaParamGeom';
 
 export * from './aaExportFile';
