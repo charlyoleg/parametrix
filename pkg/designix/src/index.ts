6ece5d6337af5893f959fa1c047f450e43138639
charles <charles.braquet@e.email>
2023-07-02 00:06:28 +0200

    add index.ts as entry point to designix

---
+++ pkg/designix/src/index.ts
@@ -0,0 +1,17 @@
+// index.ts : entry point
+
+export * from './gear_wheel_wheel';
+
+export * from './circles';
+export * from './rectangle';
+
+export * from './verify_point';
+export * from './verify_point_2';
+export * from './verify_line';
+export * from './verify_line_2';
+export * from './verify_line_3';
+export * from './verify_vector';
+export * from './verify_contour_1';
+export * from './verify_contour_2';
+export * from './verify_contour_3';
+export * from './verify_contour_4';
