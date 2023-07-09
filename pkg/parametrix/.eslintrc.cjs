2562b61b56dead5786b377b0442ab9a933dd2420
charles <charles.braquet@e.email>
2023-07-10 00:28:19 +0200

    update prettier and others

--- pkg/parametrix/.eslintrc.cjs
+++ pkg/parametrix/.eslintrc.cjs
@@ -1,17 +1,30 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:svelte/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
+		project: './tsconfig.json',
+		extraFileExtensions: ['.svelte'],
 		sourceType: 'module',
 		ecmaVersion: 2020
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	env: {
 		browser: true,
 		es2017: true,
