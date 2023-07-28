980e6007dd1d5734747f67271dee7c1e6bc74b8f
charles <charles.braquet@e.email>
2023-07-27 22:39:28 -0400

    beginning of display subDesign

--- pkg/parametrix/src/lib/SubDesign.svelte
+++ pkg/parametrix/src/lib/SubDesign.svelte
@@ -5,14 +5,24 @@
 	//import { onMount, createEventDispatcher } from 'svelte';
 	//import { browser } from '$app/environment';
 	//import { page } from '$app/stores';
-	//import { base } from '$app/paths';
+	import { base } from '$app/paths';
 
 	export let subD: tSubDesign = {};
+
+	let subInsts: Array<string> = [];
+	$: subInsts = Object.keys(subD);
 </script>
 
 <section>
 	<h2>Sub-design</h2>
-	<p>Number of sub-instances: {Object.keys(subD).length}</p>
+	<p>Number of sub-instances: {subInsts.length}</p>
+	{#each subInsts as subInst}
+		<p>
+			Instance: {subInst} ::
+			<a href="{base}/gear/{subD[subInst].partName}">{subD[subInst].partName}</a>
+			Export Constraints
+		</p>
+	{/each}
 </section>
 
 <style lang="scss">
