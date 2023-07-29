4dc51102d5d5381242af213233d2837053b01135
charles <charles.braquet@e.email>
2023-07-28 22:07:21 -0400

    adding some comment for eslintrc

--- pkg/designix/.eslintrc.cjs
+++ pkg/designix/.eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended-type-checked',
+		//'plugin:@typescript-eslint/strict-type-checked',
 		'plugin:@typescript-eslint/stylistic-type-checked',
 		'prettier'
 	],
