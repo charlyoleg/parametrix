3d6d883f64b2b577c40ea67dbbc953db302a1fb2
charles <charles.braquet@e.email>
2023-07-28 22:52:15 -0400

    upgrade typescript-eslint for parametric

--- pkg/parametrix/.eslintrc.cjs
+++ pkg/parametrix/.eslintrc.cjs
@@ -3,31 +3,33 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
+		//'plugin:@typescript-eslint/recommended-type-checked',
+		//'plugin:@typescript-eslint/strict-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
 		'plugin:svelte/recommended',
 		'prettier'
 	],
 	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-		extraFileExtensions: ['.svelte'],
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte'],
+		project: true,
+		tsconfigRootDir: __dirname
+	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
